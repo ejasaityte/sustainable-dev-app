cda00aaee0e9f1e681b0d0d1fa8f34416596ef00
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-01-24 15:35:21 +0000

    Added picture to grid

--- inc/home.php
+++ inc/home.php
@@ -55,7 +55,7 @@
                             echo '<div class="col-lg-6 col-xxl-4 mb-5">
                             <div class="card bg-light border-0 h-100">
                                 <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                    <a class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4" href="' . $row['goalPicture'] . '"></a>
                                     <h2 class="fs-4 fw-bold">' . $row['goalName'] . '</h2>
                                     <p class="mb-0">' . $row['goalDescription'] . '</p>
                                     </div>
