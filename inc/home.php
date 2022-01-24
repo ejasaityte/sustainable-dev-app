791333ce61a3efa9c5f239b615869810e6d30a31
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-01-24 16:05:59 +0000

    remove duplicates

--- inc/home.php
+++ inc/home.php
@@ -56,7 +56,7 @@
                             <div class="card bg-light border-0 h-100">
                                 <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                     <img class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4" src="' . $row['goalPicture'] . '">
-                                    <a href="/goals.php?goal=' . $row['goalID'] . '"><h2 class="fs-4 fw-bold">' . $row['goalName'] . '</h2></a><a href="#"><h2 class="fs-4 fw-bold">' . $row['goalName'] . '</h2></a>
+                                    <a href="/goals.php?goal=' . $row['goalID'] . '"><h2 class="fs-4 fw-bold">' . $row['goalName'] . '</h2></a>
                                     <p class="mb-0">' . $row['goalDescription'] . '</p>
                                     </div>
                                 </div>
