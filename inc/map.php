93f4a8dbc92b08a0bf748c5375e6ebef47fab90c
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-25 10:27:24 +0000

    map positioning adjustment

--- inc/map.php
+++ inc/map.php
@@ -26,9 +26,11 @@
                 </div>
             </div>
         </nav>
-        <div class="row gx-lg-5 align-text-center">
+        <br>
+        <div class="container-fluid text-center">
             <h1 class="display-5 fw-bold">See our locations!</h1>
         </div>
+        <br>
         <div class="row gx-lg-5 align-items-center">
             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20dundee&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
         </div>
