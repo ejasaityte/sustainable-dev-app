0d3a8d8c4b42291d34fa0342a38ead1587aef66b
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-25 16:01:29 +0000

    added redirect

--- inc/additem.php
+++ inc/additem.php
@@ -4,4 +4,5 @@ session_start();
 if(!in_array($_GET['id'], $_SESSION['favouriteslist'])){
     array_push($_SESSION['favouriteslist'], $_GET['id']);
 }
+header('location: https://sustainabledundeeapp.azurewebsites.net/');
 ?>
