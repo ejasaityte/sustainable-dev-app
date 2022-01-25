8beb51e5fc8aec5703ed27233380f1db874e7b57
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-01-25 13:13:37 +0000

    changing href

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $rules = array(
     //API Routes
     'apiShowGoals' => "/api/allGoals",
     'apiShowSingleGoal' => "/api/singleGoal/(?'goalID'[\w\-]+)",
-    'apiShowEvents' => "/api/allEvents/(?'goalID'[\w\-]+)",
+    'apiShowEvents' => "/api/allEvents",
     'map' => "/map",
 
 
