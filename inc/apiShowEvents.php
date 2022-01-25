abe012d8fdfff9d612a0c7bd0da477931e3dfcbc
laura <55388383+LauraNaslenaite@users.noreply.github.com>
2022-01-25 11:29:39 +0000

    change to events

--- inc/apiShowEvents.php
+++ inc/apiShowEvents.php
@@ -1,17 +1,13 @@
 <?php
 include("dbconnect.php");
 
-$sql = "SELECT * FROM dundee";
+$sql = "SELECT * FROM events";
 
 $rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
     $rows[] = $row;
 }
-
-  foreach($rows as $row){
-    echo $row;
-  }
 
   header('Content-type: application/json');
   echo json_encode($rows, true);
