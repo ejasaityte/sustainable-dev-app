6008d0b981178f75e149bd0528cdb933618325ac
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-01-25 16:32:53 +0000

    removed whitespace in map page

--- inc/map.php
+++ inc/map.php
@@ -39,11 +39,9 @@
                 </div>
             </div>
         </nav>
-        <br>
             <div class="container-fluid text-center">
                 <h1 class="display-5 fw-bold">See our locations!</h1>
             </div>
-            <br>
             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20dundee&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
             <!-- Footer-->
         </div>
