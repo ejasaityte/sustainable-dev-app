17f4635b2633eeac86d4abe08fd08a0e33ed11d4
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-01-25 11:28:54 +0000

    Debugging output

--- inc/apiShowEvents.php
+++ inc/apiShowEvents.php
@@ -7,8 +7,11 @@ $rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
     $rows[] = $row;
-    echo $rows;
 }
+
+  foreach($rows as $row){
+    echo $row;
+  }
 
   header('Content-type: application/json');
   echo json_encode($rows, true);
