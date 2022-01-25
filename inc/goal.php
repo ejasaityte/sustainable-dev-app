1a8a662b549cfd65a32022dd95a4586e6c5054d4
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-01-25 11:48:47 +0000

    Changed index

--- inc/goal.php
+++ inc/goal.php
@@ -31,7 +31,7 @@
             <div class="container px-lg-5">
                 <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
                     <div class="m-4 m-lg-5">
-                        <h1 class="display-5 fw-bold">A Sustainable Dundee v0.2.3</h1>
+                        <h1 class="display-5 fw-bold">A Sustainable Dundee v0.2.6</h1>
                         <p class="fs-4">Sustainability is concerned with looking after our natural environment whilst ensuring a strong economy and a fair and healthy society.</p>
                         <a class="btn btn-primary btn-lg" href="/map">Explore the map!</a>
                     </div>
@@ -45,7 +45,8 @@
                 <div class="row gx-lg-5">
 
                     <?php
-                        echo $_GET('goal');
+                        $goalNumber = $_GET('goal');
+                        echo $goalNumber;
 
                         $curl = curl_init();
 
