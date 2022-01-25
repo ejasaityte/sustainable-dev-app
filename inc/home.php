16fd1e4884e43979455c7ade3572afa21200a969
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-01-25 13:59:54 +0000

    adding changes that were undone for some reason

--- inc/home.php
+++ inc/home.php
@@ -32,7 +32,7 @@
             <div class="container px-lg-5">
                 <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
                     <div class="m-4 m-lg-5">
-                        <h1 class="display-5 fw-bold">A Sustainable Dundee v0.2.5.1</h1>
+                        <h1 class="display-5 fw-bold">A Sustainable Dundee v0.2.5.2</h1>
                         <p class="fs-4">Sustainability is concerned with looking after our natural environment whilst ensuring a strong economy and a fair and healthy society.</p>
                         <a class="btn btn-primary btn-lg" href="/map">Explore the map!</a>
                     </div>
@@ -79,7 +79,7 @@
                                 <div class="container" style="padding-bottom:10px;">
                                     <div class="row">
                                       <div class="col text-center">
-                                         <a class="btn btn-primary btn-sm mt-auto" href="/goals.php?goal=' . $goal['goalID'] . '"">Learn more</a>
+                                         <a class="btn btn-primary btn-sm mt-auto" href="/goal/' . $goal['goalID'] . '"">Learn more</a>
                                       </div>
                                     </div>
                                   </div>
