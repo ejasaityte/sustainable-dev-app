dee7f0c6b910468c155ddb4a23d5639a23aeecee
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-25 16:26:38 +0000

    bug fix

--- inc/additem.php
+++ inc/additem.php
@@ -3,8 +3,8 @@ session_start();
 
 if(!in_array($_GET['id'], $_SESSION['favouriteslist'])){
     array_push($_SESSION['favouriteslist'], $_GET['id']);
-    echo $_SESSION['favouriteslist']
-    echo $_GET['id']
+    echo $_SESSION['favouriteslist'];
+    echo $_GET['id'];
 }
 header('location: https://sustainabledundeeapp.azurewebsites.net/');
 ?>
