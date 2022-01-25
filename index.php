bc5f365ee2ebcccf3986167720743c639ae6131d
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-01-25 11:43:13 +0000

    changed index AGAIN

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ $rules = array(
     //
     // Home Page
     //
-    'home' => "/"
-    //'goal' => "/goal/(?'goal'[\w\-]+)"
+    'home' => "/",
+    'goal' => "/goal/(?'goal'[\w\-]+)"
     //
     // Style
     //
