608fe54c75c06c8bb53ad2aa1e3ba6fafb11d1f7
Nedwardio <55397842+Nedwardio@users.noreply.github.com>
2022-01-25 15:24:41 +0000

    added login

--- inc/home.php
+++ inc/home.php
@@ -28,6 +28,7 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="/map">Explore</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
                         <?php
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
           ?>
