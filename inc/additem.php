a8b01e21882a87df1fc143bca52a2d93d59b8861
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-26 16:06:22 +0000

    corrected variable name

--- inc/additem.php
+++ inc/additem.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-if(!in_array($_GET['id'], $_SESSION['favouriteslist'])){
-    array_push($_SESSION['favouriteslist'], $_GET['id']);
+if(!in_array($params['id'], $_SESSION['favouriteslist'])){
+    array_push($_SESSION['favouriteslist'], $params['id']);
 }
 header('location: https://sustainabledundeeapp.azurewebsites.net/');
 ?>
