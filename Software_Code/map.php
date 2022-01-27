b6bd5cbd8c86082a14565b3803b2d04e95f7dac7
laura <55388383+LauraNaslenaite@users.noreply.github.com>
2022-01-27 13:42:05 +0000

    removing padding for nav bar

--- Software_Code/map.php
+++ Software_Code/map.php
@@ -30,7 +30,7 @@
         <div class="main">
             <!-- Responsive navbar-->
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container px-lg-5">
+                <div class="container">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
