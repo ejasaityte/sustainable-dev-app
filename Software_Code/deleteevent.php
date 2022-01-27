a503071d5cbf3e9abe5154c71e09d33008eea860
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-27 16:22:04 +0000

    fixed css not found

--- Software_Code/deleteevent.php
+++ Software_Code/deleteevent.php
@@ -4,5 +4,5 @@ include("dbconnect.php");
 $updateReq = "DELETE FROM events WHERE id=".$params['id'];
 $updateRes =$db->query($updateReq);
 
-header('location: https://sustainabledundeeapp.azurewebsites.net/addevent');
+header('location: https://sustainabledundeeapp.azurewebsites.net');
 ?>
