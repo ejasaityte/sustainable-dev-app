f365422ab47ce52b64ee8fa1cf5dcee7da3d288a
euandocking <55397929+euandocking@users.noreply.github.com>
2022-01-27 14:20:00 +0000

    corrected explore button to correct map page

--- Software_Code/map.php
+++ Software_Code/map.php
@@ -35,7 +35,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="/map">Explore</a></li>
+                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="/map/0">Explore</a></li>
                             <?php
             if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
             ?>
