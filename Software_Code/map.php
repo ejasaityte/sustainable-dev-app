fc1aa642f837f702eab6d8c99075fd01f4cca111
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-01-27 11:49:30 +0000

    debug

--- Software_Code/map.php
+++ Software_Code/map.php
@@ -16,7 +16,7 @@
         <script src="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js"></script>
         <style>
     body { margin: 0; padding: 0; }
-    #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+    #map { position: relative; width: 100%; height: 500px; }
     </style>
     </head>
     <?php session_start(); ?>
