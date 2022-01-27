108b306716cc72a15c2468a6ea0f71390cc0bc55
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-27 13:14:14 +0000

    bugfix

--- Software_Code/deleteitem.php
+++ Software_Code/deleteitem.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
-$_SESSION['favouriteslist']=array_diff($_SESSION['favouriteslist'],$params['id']);
+
+$key=array_search($params['id'],$_SESSION['favouriteslist']);
+if($key!==false)
+    unset($_SESSION['favouriteslist'][$key]);
+$_SESSION["favouriteslist"] = array_values($_SESSION["favouriteslist"]);
+
 header('location: https://sustainabledundeeapp.azurewebsites.net/favouriteslist');
 ?>
