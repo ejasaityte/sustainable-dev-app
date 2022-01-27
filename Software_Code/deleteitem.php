a643d8629497287222c9b3aad02f54854595bc14
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-27 11:53:40 +0000

    bugfix

--- Software_Code/deleteitem.php
+++ Software_Code/deleteitem.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$key=array_search($_GET['id'],$_SESSION['favouriteslist']);
+$key=array_search($params['id'],$_SESSION['favouriteslist']);
 if($key!==false)
     unset($_SESSION['favouriteslist'][$key]);
 $_SESSION["favouriteslist"] = array_values($_SESSION["favouriteslist"]);
