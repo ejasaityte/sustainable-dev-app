0f6c4f028e43754f0d755d592c55a9a360a1c72f
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-31 15:29:08 +0000

    button fixes

--- Software_Code/goal.php
+++ Software_Code/goal.php
@@ -135,33 +135,33 @@
                     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
                         echo '
                         <div class="container" style="padding-bottom:10px;">
-                                        <div class="row">
+                                        <div class="row justify-content-center">
                                             <div class="col-sm-auto text-center">
-                                                <a class="btn btn-primary btn-sm mt-auto" href="/additem/' . $event['id'] . '"">Favourite</a>
+                                                <a class="btn btn-primary btn-sm mt-auto" style="padding-bottom:10px;" href="/additem/' . $event['id'] . '"">Favourite</a>
                                             </div>
                         ';
                         echo '
                                             <div class="col-sm-auto text-center">
-                                                <a class="btn btn-primary btn-sm mt-auto" href="/checkin/' . $_SESSION['userID'] . '"">Check in</a>
+                                                <a class="btn btn-primary btn-sm mt-auto" style="padding-bottom:10px;" href="/checkin/' . $_SESSION['userID'] . '"">Check in</a>
                                             </div>
                         ';
                         if($_SESSION['isadmin']==1){
                             echo '
                                             <div class="col-sm-auto text-center">
-                                                <a class="btn btn-primary btn-sm mt-auto" href="/edititem/' . $event['id'] . '"">Edit</a>
+                                                <a class="btn btn-primary btn-sm mt-auto" style="padding-bottom:10px;" href="/edititem/' . $event['id'] . '"">Edit</a>
                                             </div>
                                     
                         ';
                         echo '
                                             <div class="col-sm-auto text-center">
-                                                <a class="btn btn-primary btn-sm mt-auto" href="/deleteevent/' . $event['id'] . '"">Delete</a>
+                                                <a class="btn btn-primary btn-sm mt-auto" style="padding-bottom:10px;" href="/deleteevent/' . $event['id'] . '"">Delete</a>
                                             </div>
                         ';
                         }
                         echo 
                         '
                                             <div class="col-sm-auto text-center">
-                                                <a class="btn btn-primary btn-sm mt-auto" href="https://twitter.com/intent/tweet?text=I%20just%20visited%20%23' . $eventName .'%21&hashtags=sustainableDundee">Tweet</a>
+                                                <a class="btn btn-primary btn-sm mt-auto" style="padding-bottom:10px;" href="https://twitter.com/intent/tweet?text=I%20just%20visited%20%23' . $eventName .'%21&hashtags=sustainableDundee">Tweet</a>
                                             </div>
                                         </div>
                                     </div>
