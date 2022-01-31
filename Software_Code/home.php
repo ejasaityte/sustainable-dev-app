d5aea29687a25a087b169261650f6093691a29ba
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-01-31 14:55:34 +0000

    moved the images down

--- Software_Code/home.php
+++ Software_Code/home.php
@@ -101,7 +101,7 @@
                             <div class="col-lg-6 col-xxl-4 mb-5">
                                 <div class="card bg-light border-0 h-100">
                                     <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                        <img style="height: 11rem; width: 11rem;" class="feature bg-primary bg-gradient text-white mb-4 mt-n4" src="' . $goal['goalPicture'] . '">
+                                        <img style="height: 11rem; width: 11rem;" class="feature bg-primary bg-gradient text-white mb-4 mt-4" src="' . $goal['goalPicture'] . '">
                                         <h2 class="fs-4 fw-bold">' . $goal['goalName'] . '</h2></a>
                                         <p class="mb-0">' . $goal['goalDescription'] . '</p>
                                     </div>
