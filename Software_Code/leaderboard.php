9ed1ddd09629253ea6839609f60256d843e8b516
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-31 14:24:38 +0000

    bugfix on leaderboard

--- Software_Code/leaderboard.php
+++ Software_Code/leaderboard.php
@@ -93,9 +93,6 @@
                 </div>
             </div>
         </div>
-<?php
-        }
-        ?>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
