2a071d95f8ef6466613becbc1a17653276a9b1c2
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-01-31 13:15:38 +0000

    Added marker and cleaned code

--- Software_Code/map.php
+++ Software_Code/map.php
@@ -63,124 +63,134 @@
                 echo "<div id='map'></div>
                 <script>
                     mapboxgl.accessToken = 'pk.eyJ1IjoiZXVhbmRvY2tpbmciLCJhIjoiY2t5dmt1aTZvMXpqMTJxcHR2eTF2Z21zOCJ9.yZNbnAG0QP59OzGsTYFX1A';
-                const map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11',
-                center: [-2.9707, 56.4620],
-                pitchWithRotate: false,
-                trackResize: true,
-                zoom: 13
-                });
-                 
-                map.on('load', () => {
-                map.addSource('places', {
-                'type': 'geojson',
-                'data': {
-                'type': 'FeatureCollection',
-                'features': [";
+                    const map = new mapboxgl.Map({
+                        container: 'map',
+                        style: 'mapbox://styles/mapbox/streets-v11',
+                        center: [-2.9707, 56.4620],
+                        pitchWithRotate: false,
+                        trackResize: true,
+                        zoom: 13
+                    });
+                    
+                    map.on('load', () => {
+                        map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+                            (error, image) => {
+                                if (error) throw error;
+                                map.addImage('custom-marker', image);
+                                map.addSource('places', {
+                                    'type': 'geojson',
+                                    'data': {
+                                        'type': 'FeatureCollection',
+                                        'features': [";
 
-                $url = "https://sustainabledundeeapp.azurewebsites.net/api/allEventsCoords";
-                $goalID = $params['goalID'];
-                if ($goalID != 0) {
-                    $url = "https://sustainabledundeeapp.azurewebsites.net/api/EventsFromGoal/" . $goalID;
-                }
+                                            $url = "https://sustainabledundeeapp.azurewebsites.net/api/allEventsCoords";
+                                            $goalID = $params['goalID'];
+                                            if ($goalID != 0) {
+                                                $url = "https://sustainabledundeeapp.azurewebsites.net/api/EventsFromGoal/" . $goalID;
+                                            }
 
-                $curl = curl_init();
-                        curl_setopt_array($curl, array(
-                        CURLOPT_URL => $url,
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_TIMEOUT => 29,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_0_1,
-                        CURLOPT_CUSTOMREQUEST => "GET",
-                        CURLOPT_HTTPHEADER => array(
-                            "cache-control: no-cache"
-                        ),
-                        ));
+                                            $curl = curl_init();
+                                                    curl_setopt_array($curl, array(
+                                                    CURLOPT_URL => $url,
+                                                    CURLOPT_RETURNTRANSFER => true,
+                                                    CURLOPT_TIMEOUT => 29,
+                                                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_0_1,
+                                                    CURLOPT_CUSTOMREQUEST => "GET",
+                                                    CURLOPT_HTTPHEADER => array(
+                                                        "cache-control: no-cache"
+                                                    ),
+                                                    ));
 
-                        $response = curl_exec($curl);
-                        $err = curl_error($curl);
+                                                    $response = curl_exec($curl);
+                                                    $err = curl_error($curl);
 
-                        curl_close($curl);
-                        // Decode JSON data into PHP array
-                        $response = json_decode($response, true);
+                                                    curl_close($curl);
+                                                    // Decode JSON data into PHP array
+                                                    $response = json_decode($response, true);
 
-                        $i = 0;
-                        foreach ($response as $event) { // TODO refactor
-                            $i += 1;
-                            echo "
-                            {
-                            'type': 'Feature',
-                            'properties': {
-                                'description':
-                                '<strong> " . $event['name'] . " </strong><p>" . $event['description'] . "</p><strong>For more information visit the <a href=" . $event['website'] . ">website</a></strong><p><strong>Contact</strong>: " . $event['contacts'] . "</p>'
-                            },
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [" . $event['lon'] . ", " . $event['lat'] . "]
+                                                    $i = 0;
+                                                    foreach ($response as $event) { // TODO refactor
+                                                        $i += 1;
+                                                        echo "
+                                                        {
+                                                        'type': 'Feature',
+                                                        'properties': {
+                                                            'description':
+                                                            '<strong> " . $event['name'] . " </strong><p>" . $event['description'] . "</p><strong>For more information visit the <a href=" . $event['website'] . ">website</a></strong><p><strong>Contact</strong>: " . $event['contacts'] . "</p>'
+                                                        },
+                                                        'geometry': {
+                                                            'type': 'Point',
+                                                            'coordinates': [" . $event['lon'] . ", " . $event['lat'] . "]
+                                                        }
+                                                        }";
+                                                        if ($i != count($response)) {
+                                                            echo ","; 
+                                                        }
+                                                    }
+                                            echo "
+                                        ]
+                                    }
+                                });
+
+                                // Add a layer showing the places.
+                                map.addLayer({
+                                    'id': 'places',
+                                    'type': 'circle',
+                                    'source': 'places',
+                                    'paint': {
+                                        'circle-color': '#4264fb',
+                                        'circle-radius': 6,
+                                        'circle-stroke-width': 2,
+                                        'circle-stroke-color': '#ffffff'
+                                    }
+                                });
                             }
-                            }";
-                            if ($i != count($response)) {
-                                echo ","; 
-                            }
+                        );
+                    });
+                    
+                    // Create a popup, but don't add it to the map yet.
+                    const popup = new mapboxgl.Popup({
+                        closeButton: false,
+                        closeOnClick: true
+                    });
+                    
+                    map.on('click', 'places', (e) => {
+                        // Change the cursor style as a UI indicator.
+                        map.getCanvas().style.cursor = 'pointer';
+                    
+                        // Copy coordinates array.
+                        const coordinates = e.features[0].geometry.coordinates.slice();
+                        const description = e.features[0].properties.description;
+                    
+                        // Ensure that if the map is zoomed out such that multiple
+                        // copies of the feature are visible, the popup appears
+                        // over the copy being pointed to.
+                        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                         }
-                echo "
-                ]
-                }
-                });
-                // Add a layer showing the places.
-                map.addLayer({
-                'id': 'places',
-                'type': 'circle',
-                'source': 'places',
-                'paint': {
-                'circle-color': '#4264fb',
-                'circle-radius': 6,
-                'circle-stroke-width': 2,
-                'circle-stroke-color': '#ffffff'
-                }
-                });
-                 
-                // Create a popup, but don't add it to the map yet.
-                const popup = new mapboxgl.Popup({
-                closeButton: false,
-                closeOnClick: true
-                });
-                 
-                map.on('click', 'places', (e) => {
-                // Change the cursor style as a UI indicator.
-                map.getCanvas().style.cursor = 'pointer';
-                 
-                // Copy coordinates array.
-                const coordinates = e.features[0].geometry.coordinates.slice();
-                const description = e.features[0].properties.description;
-                 
-                // Ensure that if the map is zoomed out such that multiple
-                // copies of the feature are visible, the popup appears
-                // over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
-                 
-                // Populate the popup and set its coordinates
-                // based on the feature found.
-                popup.setLngLat(coordinates).setHTML(description).addTo(map);
-                });
-                map.on('mouseleave', 'places', () => {
-                    map.getCanvas().style.cursor = '';
+                    
+                        // Populate the popup and set its coordinates
+                        // based on the feature found.
+                        popup.setLngLat(coordinates).setHTML(description).addTo(map);
                     });
+
+                    map.on('mouseleave', 'places', () => {
+                        map.getCanvas().style.cursor = '';
                     });
-                // Add geolocate control to the map.
-                map.addControl(
-                new mapboxgl.GeolocateControl({
-                    positionOptions: {
-                        enableHighAccuracy: true
-                    },
-                    // When active the map will receive updates to the device's location as it changes.
-                    trackUserLocation: true,
-                    // Draw an arrow next to the location dot to indicate which direction the device is heading.
-                    showUserHeading: true
-                    })
-                );
+
+                    // Add geolocate control to the map.
+                    map.addControl(
+                        new mapboxgl.GeolocateControl({
+                            positionOptions: {
+                                enableHighAccuracy: true
+                            },
+                            // When active the map will receive updates to the device's location as it changes.
+                            trackUserLocation: true,
+                            // Draw an arrow next to the location dot to indicate which direction the device is heading.
+                            showUserHeading: true
+                        })
+                    );
+                    
                 </script>";
             php?>
             <!-- Footer-->
