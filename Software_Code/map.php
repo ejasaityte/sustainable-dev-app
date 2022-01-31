160a24bd260cecb8cecc6f099cab6d7a03e293b3
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-01-31 16:06:45 +0000

    map check in functionality added

--- Software_Code/map.php
+++ Software_Code/map.php
@@ -119,12 +119,13 @@
                                                         'type': 'Feature',
                                                         'properties': {
                                                             'description':
-                                                            '<strong> " . $event['name'] . " </strong><p>" . $event['description'] . "</p><strong>For more information visit the <a href=" . $event['website'] . ">website</a></strong><p><strong>Contact</strong>: " . $event['contacts'] . "</p>'";
+                                                            '<strong> " . $event['name'] . " </strong><p>" . $event['description'] . "</p><strong>For more information visit the <a href=" . $event['website'] . ">website</a></strong><p><strong>Contact</strong>: " . $event['contacts'] . "</p>";
                                                             if($_SESSION['loggedin'] == true){
                                                             echo "<div class=\'col-sm-auto text-center\' style=\'padding-bottom:10px;\'>
                                                                     <a class=\'btn btn-primary btn-sm mt-auto\' href=\'/checkin/" . $_SESSION['userID'] . "\'>Check in</a>
                                                                  </div>";
                                                             }
+                                                            echo "'";
                                                         echo "},
                                                         'geometry': {
                                                             'type': 'Point',
