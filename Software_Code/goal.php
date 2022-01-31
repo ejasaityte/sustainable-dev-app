24a5a912502e05136f0f1fa0c892626d65005c6a
laura <55388383+LauraNaslenaite@users.noreply.github.com>
2022-01-31 16:22:02 +0000

    image as a button

--- Software_Code/goal.php
+++ Software_Code/goal.php
@@ -168,7 +168,7 @@
                         echo 
                                 '
                                             <div class="col-sm-auto  text-center" style="padding-bottom:10px;">
-                                            <a class="btn btn-primary btn-sm mt-auto" href="https://twitter.com/intent/tweet?text=I%20just%20visited%20%23' . $name . '%21&hashtags=sustainableDundee" target="_blank"><img src="twitterLogo.png" width="32" height="32">
+                                            <a href="https://twitter.com/intent/tweet?text=I%20just%20visited%20%23' . $name . '%21&hashtags=sustainableDundee" target="_blank"><img src="ejasaityte/sustainable-dev-app/Software_Code/twitterLogo.png" width="32" height="32">
                                             </a>                                           
                                             </div>
                                         </div>
