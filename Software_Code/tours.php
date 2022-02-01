a69ed1455304a60bafd293e1e694451aae2cf7c8
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-02-01 15:36:19 +0000

    added tour map

--- Software_Code/tours.php
+++ Software_Code/tours.php
@@ -73,9 +73,153 @@
             <div class="container px-lg-5">
                 <!-- Page Features-->
                 <div class="row gx-lg-5 pt-3">
+                    <h1 class="display-5 fw-bold">See our locations!</h1>
+                    <?php
+                        echo "<div id='map'></div>
+                        <script>
+                            mapboxgl.accessToken = 'pk.eyJ1IjoiZXVhbmRvY2tpbmciLCJhIjoiY2t5dmt1aTZvMXpqMTJxcHR2eTF2Z21zOCJ9.yZNbnAG0QP59OzGsTYFX1A';
+                            const map = new mapboxgl.Map({
+                                container: 'map',
+                                style: 'mapbox://styles/mapbox/streets-v11',
+                                center: [-2.9707, 56.4620],
+                                pitchWithRotate: false,
+                                trackResize: true,
+                                zoom: 13
+                            });
+                            
+                            map.on('load', () => {
+                                map.loadImage('https://i.imgur.com/lz3uxL1.png',
+                                    (error, image) => {
+                                        if (error) throw error;
+                                        map.addImage('custom-marker', image);
+                                        map.addSource('places', {
+                                            'type': 'geojson',
+                                            'data': {
+                                                'type': 'FeatureCollection',
+                                                'features': [";
 
-                    
+                                                    $url = "https://sustainabledundeeapp.azurewebsites.net/api/allEventsCoords";
+                                                    $goalID = $params['goalID'];
+                                                    if ($goalID != 0) {
+                                                        $url = "https://sustainabledundeeapp.azurewebsites.net/api/EventsFromGoal/" . $goalID;
+                                                    }
 
+                                                    $curl = curl_init();
+                                                            curl_setopt_array($curl, array(
+                                                            CURLOPT_URL => $url,
+                                                            CURLOPT_RETURNTRANSFER => true,
+                                                            CURLOPT_TIMEOUT => 29,
+                                                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_0_1,
+                                                            CURLOPT_CUSTOMREQUEST => "GET",
+                                                            CURLOPT_HTTPHEADER => array(
+                                                                "cache-control: no-cache"
+                                                            ),
+                                                            ));
+
+                                                            $response = curl_exec($curl);
+                                                            $err = curl_error($curl);
+
+                                                            curl_close($curl);
+                                                            // Decode JSON data into PHP array
+                                                            $response = json_decode($response, true);
+
+                                                            $i = 0;
+                                                            foreach ($response as $event) { // TODO refactor
+                                                                $i += 1;
+                                                                echo "
+                                                                {
+                                                                'type': 'Feature',
+                                                                'properties': {
+                                                                    'description':
+                                                                    '<strong> " . $event['name'] . " </strong><p>" . $event['description'] . "</p><strong>For more information visit the <a href=" . $event['website'] . ">website</a></strong><p><strong>Contact</strong>: " . $event['contacts'] . "</p><p><strong>Address</strong>: " . $event['address'] . "</p>";
+
+                                                                    if($_SESSION['loggedin'] == true){
+                                                                    echo "<div class=\'col-sm-auto text-center\' style=\'padding-bottom:10px;\'\> <a class=\'btn btn-primary btn-sm mt-auto\' href=\'/checkin/" . $_SESSION['userID'] . "\'>Check in</a></div\>";
+                                                                    if (str_word_count($event['name']) > 1)
+                                                                    {
+                                                                        $str = explode(" ", $event['name']);
+                                                                        $name = implode("", $str);
+                                                                    }
+                                                                    else {
+                                                                        $name = $event['name'];
+                                                                    }
+                                                                    echo 
+                                                                        "<div class=\'col-sm-auto  text-center\' style=\'padding-bottom:10px;\'\><a data-bs-toggle=\'tooltip\' data-bs-placement=\'right\' title=\'Tweet\' href=\'https://twitter.com/intent/tweet?text=I%20just%20visited%20%23" . $name . "%21%20@DundeeCouncil&hashtags=sustainabledundee\' target=\'_blank\'\><img src=\'https://logos-world.net/wp-content/uploads/2020/04/Twitter-Emblem.png\'  style=\'width:50px;height:28px;\'\></a></div></div></div>";
+                                                                    }
+                                                                echo "'},
+                                                                'geometry': {
+                                                                    'type': 'Point',
+                                                                    'coordinates': [" . $event['lon'] . ", " . $event['lat'] . "]
+                                                                }
+                                                                }";
+                                                                if ($i != count($response)) {
+                                                                    echo ","; 
+                                                                }
+                                                            }
+                                                    echo "
+                                                ]
+                                            }
+                                        });
+
+                                        // Add a layer showing the places.
+                                        map.addLayer({
+                                            'id': 'places',
+                                            'type': 'symbol',
+                                            'source': 'places',
+                                            'layout': {
+                                                'icon-image': 'custom-marker',
+                                            }
+                                        });
+                                    }
+                                );
+                            });
+                            
+                            // Create a popup, but don't add it to the map yet.
+                            const popup = new mapboxgl.Popup({
+                                closeButton: false,
+                                closeOnClick: true
+                            });
+                            
+                            map.on('click', 'places', (e) => {
+                                // Change the cursor style as a UI indicator.
+                                map.getCanvas().style.cursor = 'pointer';
+                            
+                                // Copy coordinates array.
+                                const coordinates = e.features[0].geometry.coordinates.slice();
+                                const description = e.features[0].properties.description;
+                            
+                                // Ensure that if the map is zoomed out such that multiple
+                                // copies of the feature are visible, the popup appears
+                                // over the copy being pointed to.
+                                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                                }
+                            
+                                // Populate the popup and set its coordinates
+                                // based on the feature found.
+                                popup.setLngLat(coordinates).setHTML(description).addTo(map);
+                            });
+
+                            map.on('mouseleave', 'places', () => {
+                                map.getCanvas().style.cursor = '';
+                            });
+
+                            // Add geolocate control to the map.
+                            map.addControl(
+                                new mapboxgl.GeolocateControl({
+                                    positionOptions: {
+                                        enableHighAccuracy: true
+                                    },
+                                    // When active the map will receive updates to the device's location as it changes.
+                                    trackUserLocation: true,
+                                    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+                                    showUserHeading: true
+                                })
+                            );
+                            
+                        </script>";
+                    php?>
+                    <!-- Footer-->
                 </div>
             </div>
         </section>
