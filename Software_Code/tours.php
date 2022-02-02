da3551ab04acbb416c1c9094b75a36f6bd76822d
Matt Bain <55392423+matt-bain@users.noreply.github.com>
2022-02-02 16:03:41 +0000

    fix bounding

--- Software_Code/tours.php
+++ Software_Code/tours.php
@@ -155,7 +155,7 @@
                         function isElementOnScreen(id) {
                             const element = document.getElementById(id);
                             const bounds = element.getBoundingClientRect();
-                            return bounds.top < document.getElementById('features').innerHeight && bounds.bottom > 0;
+                            return bounds.top < document.getElementById('features').clientHeight && bounds.bottom > 0;
                         }
                                 
                         // On every scroll event, check which element is on screen
