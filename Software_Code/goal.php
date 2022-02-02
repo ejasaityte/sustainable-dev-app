29b2c86dffcec6d4f1a933cceb51528af0411219
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-02-02 11:06:16 +0000

    More refactoring

--- Software_Code/goal.php
+++ Software_Code/goal.php
@@ -62,7 +62,6 @@
         } 
 
 ?>
-
                         <li class="nav-item"><a class="nav-link" href="/logout">Log out</a></li>
 <?php 
     } else{
@@ -84,34 +83,26 @@
                     <div class="m-4 m-lg-5">
 <?php
     $goalID = $params['goal'];
-
     $curl = curl_init();
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://sustainabledundeeapp.azurewebsites.net/api/singleGoal/$goalID",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_TIMEOUT => 29,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_0_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-        "cache-control: no-cache"
-    ),
-    ));
-
+    curl_setopt_array(
+        $curl,
+        array(
+            CURLOPT_URL => "https://sustainabledundeeapp.azurewebsites.net/api/singleGoal/$goalID",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 29,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_0_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache"
+            ),
+        )
+    );
     $response = curl_exec($curl);
     $err = curl_error($curl);
-
     curl_close($curl);
-    // Decode JSON data into PHP array
-    $response = json_decode($response, true);
-    print_r($response);
+    $response = json_decode($response, true);// Decode JSON data into PHP array
     echo '<h1 class="display-5 fw-bold">'.$response[0]['goalName'].'</h1>';
     echo '<p class="fs-4">'.$response[0]['goalDescription'].'</p>';
-    /*foreach ($response as $goal) { // TODO refactor
-        echo '<h1 class="display-5 fw-bold">'.$goal['goalName'].'</h1>';
-        echo '<p class="fs-4">'.$goal['goalDescription'].'</p>';
-        break; // This should run once
-    }*/
-    
     echo "<a class='btn btn-primary btn-lg' href='/map/" . $goalID . "'>Explore the map!</a>";
 ?>
                     </div>
@@ -125,69 +116,59 @@
                 <div class="row gx-lg-5 pt-3">
 
 <?php
-    
     $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://sustainabledundeeapp.azurewebsites.net/api/allEvents/$goalID",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-        "cache-control: no-cache"
-    ),
-    ));
-
+    curl_setopt_array($curl, 
+        array(
+            CURLOPT_URL => "https://sustainabledundeeapp.azurewebsites.net/api/allEvents/$goalID",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache"
+            ),
+        )
+    );
     $response = curl_exec($curl);
     $err = curl_error($curl);
-
     curl_close($curl);
-    // Decode JSON data into PHP array
-    $response = json_decode($response, true);
-    
+    $response = json_decode($response, true);// Decode JSON data into PHP array
     foreach ($response as $event) {
-        echo '
-        <div class="col-lg-6 col-xxl-4 mb-5">
-            <div class="card bg-light border-0 h-100">
-                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                    <h2 class="fs-4 fw-bold">' . $event['name'] . '</h2></a>
-                    <p class="mb-0">' . $event['description'] . '</p>
-                    <p> </p>
-                    <p class="text-start"><strong>For more information visit the <a href=' . $event['website'] . '>website</a></strong></p> 
-                    <p class="text-start"><strong>Contact</strong>: ' . $event['contacts'] . '</p>
+?>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                <h2 class="fs-4 fw-bold">' . $event['name'] . '</h2></a>
+                                <p class="mb-0">' . $event['description'] . '</p>
+                                <p> </p>
+                                <p class="text-start"><strong>For more information visit the <a href=' . $event['website'] . '>website</a></strong></p> 
+                                <p class="text-start"><strong>Contact</strong>: ' . $event['contacts'] . '</p>
 
-                </div>';
+                            </div>
+<?php
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-            echo '
-            <div class="container" style="padding-bottom:10px;">
-                            <div class="row justify-content-center">
-                                <div class="col-sm-auto text-center" style="padding-bottom:10px;">
-                                    <a class="btn btn-primary btn-sm mt-auto" href="/additem/' . $event['id'] . '"">Favourite</a>
-                                </div>
-            ';
-            echo '
-                                <div class="col-sm-auto text-center" style="padding-bottom:10px;">
-                                    <a class="btn btn-primary btn-sm mt-auto" href="/checkin/' . $_SESSION['userID'] . '"">Check in</a>
-                                </div>
-            ';
+?>
+                            <div class="container" style="padding-bottom:10px;">
+                                <div class="row justify-content-center">
+                                    <div class="col-sm-auto text-center" style="padding-bottom:10px;">
+                                        <a class="btn btn-primary btn-sm mt-auto" href="/additem/' . $event['id'] . '"">Favourite</a>
+                                    </div>
+                                    <div class="col-sm-auto text-center" style="padding-bottom:10px;">
+                                        <a class="btn btn-primary btn-sm mt-auto" href="/checkin/' . $_SESSION['userID'] . '"">Check in</a>
+                                    </div>
+<?php
             if($_SESSION['isadmin']==1){
-                echo '
-                                <div class="col-sm-auto text-center" style="padding-bottom:10px;">
-                                    <a class="btn btn-primary btn-sm mt-auto" href="/edititem/' . $event['id'] . '"">Edit</a>
-                                </div>
-                        
-            ';
-            echo '
-                                <div class="col-sm-auto text-center" style="padding-bottom:10px;">
-                                    <a class="btn btn-primary btn-sm mt-auto" href="/deleteevent/' . $event['id'] . '"">Delete</a>
-                                </div>
-            ';
-            echo '
-                                <div class="col-sm-auto text-center" style="padding-bottom:10px;">
-                                    <a class="btn btn-primary btn-sm mt-auto" href="/editlocation/' . $event['id'] . '"">Edit location</a>
-                                </div>
-            ';
+?>
+                                    <div class="col-sm-auto text-center" style="padding-bottom:10px;">
+                                        <a class="btn btn-primary btn-sm mt-auto" href="/edititem/' . $event['id'] . '"">Edit</a>
+                                    </div>
+                                    <div class="col-sm-auto text-center" style="padding-bottom:10px;">
+                                        <a class="btn btn-primary btn-sm mt-auto" href="/deleteevent/' . $event['id'] . '"">Delete</a>
+                                    </div>
+                                    <div class="col-sm-auto text-center" style="padding-bottom:10px;">
+                                        <a class="btn btn-primary btn-sm mt-auto" href="/editlocation/' . $event['id'] . '"">Edit location</a>
+                                    </div>
+<?php
             }
             if (str_word_count($event['name']) > 1)
             {
@@ -197,24 +178,21 @@
             else {
                 $name = $event['name'];
             }
-            echo 
-                    '
-                                <div class="col-sm-auto  text-center" style="padding-bottom:10px;">
-                                <a data-bs-toggle="tooltip" data-bs-placement="right" title="Tweet" href="https://twitter.com/intent/tweet?text=I%20just%20visited%20%23' . $name . '%21&hashtags=sustainabledundee" target="_blank"><img src="https://logos-world.net/wp-content/uploads/2020/04/Twitter-Emblem.png"  style="width:50px;height:28px;">
-                                </a>                                           
+?>
+                                    <div class="col-sm-auto  text-center" style="padding-bottom:10px;">
+                                        <a data-bs-toggle="tooltip" data-bs-placement="right" title="Tweet" href="https://twitter.com/intent/tweet?text=I%20just%20visited%20%23' . $name . '%21&hashtags=sustainabledundee" target="_blank"><img src="https://logos-world.net/wp-content/uploads/2020/04/Twitter-Emblem.png"  style="width:50px;height:28px;">
+                                        </a>                                           
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-            ';
+<?php
         } 
-
-        echo '
-            </div>
-        </div>';
-        
+?>
+                        </div>
+                    </div>
+<?php    
     }
 ?>
-
                 </div>
             </div>
         </section>
