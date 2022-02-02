58e94332eb258958cbdf147c4ab2851c854eea3b
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-02-02 10:31:06 +0000

    fixed delete event button to also remove the event's coord entry

--- Software_Code/deleteevent.php
+++ Software_Code/deleteevent.php
@@ -1,6 +1,20 @@
 <?php
 include("dbconnect.php");
 
+$updateReq = "SELECT postcode FROM events WHERE id=".$params['id'];
+
+$rows = array();
+$result = $db->query($updateReq);
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+}
+
+foreach ($rows as $row)
+{
+    $updateReq = "DELETE FROM coord WHERE postcode='".$row['postcode']."'";
+    $updateRes =$db->query($updateReq);
+}
+
 $updateReq = "DELETE FROM events WHERE id=".$params['id'];
 $updateRes =$db->query($updateReq);
 
