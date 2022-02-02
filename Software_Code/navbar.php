c15bfb1640143bf4e2cf66e0094eb9759a31af0d
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-02-02 15:23:49 +0000

    Improved nav bar

--- Software_Code/navbar.php
+++ Software_Code/navbar.php
@@ -13,11 +13,11 @@
         $rows = array();
         $result = $db->query($sql);
         while ($row = $result->fetch_assoc()) $rows[] = $row;
-        if (empty($rows)) echo '<li class="nav-item" id="friends"><a class="nav-link " href="/addfriend">Friends</a></li>';
-        else echo '<li class="nav-item" id="friends"><a class="nav-link viridian" href="/addfriend">Friends</a></li>';
-        if ($_SESSION['username']=='admin') echo '<li class="nav-item" id="addevent"><a class="nav-link" href="/addevent">Add Event</a></li><li class="nav-item" id="adduser"><a class="nav-link" href="/adduser">Add User</a></li>';
-        echo '<li class="nav-item" id="favouriteslist"><a class="nav-link" href="/favouriteslist">Favourites</a></li><li class="nav-item" id="logout"><a class="nav-link" href="/logout">Log out</a></li>';
-    } else echo '<li class="nav-item" id="login"><a class="nav-link" href="/login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li><li class="nav-item" id="register"><a class="nav-link" href="/register">Register</a></li>';
+        if (empty($rows)) echo '<li class="nav-item"><a class="nav-link"  id="friends" href="/addfriend">Friends</a></li>';
+        else echo '<li class="nav-item"><a class="nav-link viridian" id="friends" href="/addfriend">Friends</a></li>';
+        if ($_SESSION['username']=='admin') echo '<li class="nav-item"><a class="nav-link" id="addevent" href="/addevent">Add Event</a></li><li class="nav-item"><a class="nav-link" id="adduser" href="/adduser">Add User</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" id="favouriteslist" href="/favouriteslist">Favourites</a></li><li class="nav-item"><a class="nav-link" id="logout" href="/logout">Log out</a></li>';
+    } else echo '<li class="nav-item"><a class="nav-link"  id="login" href="/login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li><li class="nav-item"><a class="nav-link" id="register" href="/register">Register</a></li>';
 ?>
             </ul>
         </div>
