4d2b7e576ff120f6bb3971c9bba787165672791a
Nedwardio <55397842+Nedwardio@users.noreply.github.com>
2022-02-02 15:36:51 +0000

    Update adduser.php

--- Software_Code/adduser.php
+++ Software_Code/adduser.php
@@ -99,13 +99,13 @@ element.classList.add("active");
     ?>
 
     </div>
-            <!-- footer -->
+    
+            
+    </div>
+        <!-- footer -->
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Sustainable Dundee 2021</p></div>
         </footer>
-            
-    </div>
-        
         
         
         <!-- Bootstrap core JS-->
