ba9419b550519bc4938ae2b6565e3a57d7c150f8
Jamie McTaggart <mctaggartjamie62@gmail.com>
2022-02-02 11:52:02 +0000

    refactored register page

--- Software_Code/register.php
+++ Software_Code/register.php
@@ -14,91 +14,60 @@
         <link href="css/styles.css" rel="stylesheet" />
     </head>
 <body>
-    <?php
+<?php
     session_start();
     include("dbconnect.php");
-    
     error_reporting(0);
-
-  ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-lg-5">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link"href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/map/0">Explore</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/news">News</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/leaderboard">Leaderboard</a></li>
-                        <li class="nav-item"><a class="nav-link"href="/login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="/register">Register</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <form action="" method="post">
-            <div class="d-grid gap-2">
-                <div class="input-group">
-                    <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
-                    <div class="invalid-tooltip"> Please enter an email.</div>
-                </div>
-                <div class="input-group">
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password" aria-describedby="passwordHelpBlock" required>
-                    <div class="invalid-tooltip"> Please enter a password.</div>
-                </div>
-                <button type="submit" class="btn btn-primary">Register</button>
-            </div>
-
-
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container px-lg-5">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link"href="/">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/map/0">Explore</a></li>
+                <li class="nav-item"><a class="nav-link" href="/news">News</a></li>
+                <li class="nav-item"><a class="nav-link" href="/leaderboard">Leaderboard</a></li>
+                <li class="nav-item"><a class="nav-link"href="/login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+                <li class="nav-item"><a class="nav-link active" href="/register">Register</a></li>
+            </ul>
+        </div>
     </div>
-    </div>
-
+</nav>
+<div class="position-absolute top-50 start-50 translate-middle">
+    <form action="" method="post">
+        <div class="d-grid gap-2">
+            <div class="input-group">
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+                <div class="invalid-tooltip"> Please enter an email.</div>
+            </div>
+            <div class="input-group">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" aria-describedby="passwordHelpBlock" required>
+                <div class="invalid-tooltip"> Please enter a password.</div>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </div>
     </form>
-    <?php
-        $username = $_POST['username'];
-        $password = $_POST['password'];  
-        if(($username=="admin")||(!(preg_match("/@/", $username))&&($username!='')))
-        {
-            ?><div class="alert alert-warning" role="alert">
-  Invalid email!
-</div> <?php
-        }
-        else {
-            $sql = "SELECT * FROM users WHERE email='". $username ."'";
-            $rows = array();
-            $result = $db->query($sql);
-            while ($row = $result->fetch_assoc()) {
-                $rows[] = $row;
-
-            }
-
-            if(empty($rows)){
-                $hashedPass = password_hash($password,PASSWORD_DEFAULT);
-                if(strlen($username)!=0)
-                {$updateReq = "INSERT INTO users (userID, email, password, admin, leaderboard, eventOrganiser) VALUES (NULL,'".$username."','".$hashedPass."',0,0,0)";
+</div>
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];  
+    if(($username=="admin")||(!(preg_match("/@/", $username))&&($username!=''))) echo '<div class="alert alert-warning" role="alert">Invalid email!</div>';
+    else {
+        $sql = "SELECT * FROM users WHERE email='". $username ."'";
+        $rows = array();
+        $result = $db->query($sql);
+        while ($row = $result->fetch_assoc()) $rows[] = $row;
+        if(empty($rows)){
+            $hashedPass = password_hash($password,PASSWORD_DEFAULT);
+            if(strlen($username)!=0){
+                $updateReq = "INSERT INTO users (userID, email, password, admin, leaderboard, eventOrganiser) VALUES (NULL,'".$username."','".$hashedPass."',0,0,0)";
                 $updateRes =$db->query($updateReq);
-                ?><div class="alert alert-warning" role="alert">
-    Successfully registered!
-    </div> <?php
+                echo '<div class="alert alert-warning" role="alert">Successfully registered!</div>';
             }
         }
-            elseif ($username!="") {
-                ?><div class="alert alert-warning" role="alert">
-    Email already registered!
-    </div> <?php
-            }
+        elseif ($username!="") echo '<div class="alert alert-warning" role="alert">Email already registered!</div>';
     }
-    ?>
-    </div>
-
-
+?>
 </body>
-
 </html>
-
-
-
-<!-- https://www.bootstrapdash.com/use-bootstrap-with-php/ -->
