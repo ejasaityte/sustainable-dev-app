85737f75093dd2025322845c916de1aa58df512c
Nedwardio <55397842+Nedwardio@users.noreply.github.com>
2022-02-02 14:57:29 +0000

    test

--- Software_Code/changepassword.php
+++ Software_Code/changepassword.php
@@ -119,13 +119,12 @@
     </div>
         
         <!-- footer -->
-        <br>
-            <br>
+        <br><br><br><br>
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Sustainable Dundee 2021</p></div>
         </footer>
-        </br>
-    </br>
+        </br></br></br></br>
+        
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
