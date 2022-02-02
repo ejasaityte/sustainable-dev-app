8e043f7b65e434c1a41987166836d6a8c51ff7f9
Nedwardio <55397842+Nedwardio@users.noreply.github.com>
2022-02-02 16:41:36 +0000

    tweak position

--- Software_Code/favouriteslist.php
+++ Software_Code/favouriteslist.php
@@ -117,6 +117,7 @@ element.classList.add("active");
         <style>
             .footer {
                 height: 100px;
+                position: fixed;
                 display: flex;
                 left: 0;
                 bottom: 0;
