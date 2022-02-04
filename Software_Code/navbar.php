da389bb7bab12a9e34fcbbfc8fbd323ad7fbbe5b
Sem <55398111+ejasaityte@users.noreply.github.com>
2022-02-04 11:09:44 +0000

    fixed addevent showing

--- Software_Code/navbar.php
+++ Software_Code/navbar.php
@@ -19,8 +19,8 @@
         while ($row = $result->fetch_assoc()) $rows[] = $row;
         if (empty($rows)) echo '<li class="nav-item"><a class="nav-link"  id="friends" href="/addfriend">Friends</a></li>';
         else echo '<li class="nav-item"><a class="nav-link viridian" id="friends" href="/addfriend">Friends</a></li>';
-        if ($_SESSION['username']=='admin') echo '<li class="nav-item"><a class="nav-link" id="addevent" href="/addevent">Add Event</a></li><li class="nav-item"><a class="nav-link" id="addtour" href="/addtour">Add Tour</a></li><li class="nav-item"><a class="nav-link" id="adduser" href="/adduser">Add User</a></li>';
-        echo '<li class="nav-item"><a class="nav-link" id="favouriteslist" href="/favouriteslist">Favourites</a></li><li class="nav-item"><a class="nav-link" id="changepassword" href="/changepassword">Change password</a></li><li class="nav-item"><a class="nav-link" id="logout" href="/logout">Log out</a></li>';
+        if ($_SESSION['username']=='admin') echo '<li class="nav-item"><a class="nav-link" id="addtour" href="/addtour">Add Tour</a></li><li class="nav-item"><a class="nav-link" id="adduser" href="/adduser">Add User</a></li>';
+        echo '<li class="nav-item"><a class="nav-link" id="addevent" href="/addevent">Add Event</a></li><li class="nav-item"><a class="nav-link" id="favouriteslist" href="/favouriteslist">Favourites</a></li><li class="nav-item"><a class="nav-link" id="changepassword" href="/changepassword">Change password</a></li><li class="nav-item"><a class="nav-link" id="logout" href="/logout">Log out</a></li>';
     } else echo '<li class="nav-item"><a class="nav-link"  id="login" href="/login"><span class="glyphicon glyphicon-log-in"></span>Login</a></li><li class="nav-item"><a class="nav-link" id="register" href="/register">Register</a></li>';
 ?>
             </ul>
